2c47dfa80edf77932fa344a1fba71acb1005a96e
imithu <imithu@imithu.org>
2021-06-21 23:04:41 +0600

    💄 add - header, footer and menus have been added

---
+++ components/layouts/default/content/menus.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+
+    <div class='list-group border-0'>
+      <nuxt-link 
+        class='list-group-item rounded-0 border-success bg-transparent link-hover text-light'
+        v-for='(e, i) in menus'
+        :key='i'
+        :to='e.path'
+        :class="{ active: ($route.path===e.path) ? true : false }"
+        >
+        {{e.name}}
+      </nuxt-link>
+    </div>
+
+  </div>
+</template>
+
+
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  data(){
+    return {
+      menus: [
+        {
+          name: 'Installation',
+          path: '/docs/installation'
+        },
+        {
+          name: 'Notification',
+          path: '/docs/notification'
+        },
+        {
+          name: 'Cursor',
+          path: '/docs/cursor'
+        }
+      ]
+    }
+  }
+})
+</script>
+
+
+
+
+
+<style scoped>
+.link-hover:hover, .active {
+  background-color: var(--bs-success) !important;
+}
+</style>
---
+++ components/layouts/default/navigation/top.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+
+    <header class="p-4 bg-success">
+      <div class='text-center fs-3'>Afancy Documentation</div>
+      <div class="text-center fs-6">Version: 1.0.0</div>
+      <div class='text-center mt-2'>
+        <a href="https://github.com/afancy-org/afancy" target="_blank" class='mx-1'><img src="/img/logo/GitHub-Mark-Light-120px-plus.png" alt="github logo" style='max-width: 32px;'></a>
+        <a href="https://discord.gg/HTJtTQNc35"        target="_blank" class='mx-1'><img src="/img/logo/Discord-Logo-White.svg" alt="discord logo" style='max-width: 33px;'></a>
+      </div>
+    </header>
+
+
+  </div>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
