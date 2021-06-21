f5d9882d005d9533c5415da09a24d291dbac0288
imithu <imithu@imithu.org>
2021-06-21 23:07:44 +0600

    📝 add - notification documentation has been added

---
+++ pages/docs/notification.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+
+    <div class="h1 text-center text-primary" style='font-family: Fredericka the Great;'>Notification</div>
+
+    <div class="btn-group mb-2">
+      <button @click="ntfc('neutral', 'Hi, This is a neutral notification example. This is default notification without adding additional css.')" class="btn btn-outline-primary">Neutral</button>
+      <button @click="ntfc('success', 'Hi, This is a success notification example. This is default notification without adding additional css.')" class="btn btn-outline-success">Success</button>
+      <button @click="ntfc('error',   'Hi, This is a error   notification example. This is default notification without adding additional css.')" class="btn btn-outline-danger">Error</button>
+    </div>
+
+    <ntfc ref='ntfc' />
+
+    <hljs lang='html'>
+    &lt;!-- template --&gt;
+    &lt;ntfc ref=&apos;ntfc&apos; /&gt;
+    </hljs>
+
+    <hljs class='mt-2' lang='js'>
+    // method
+    this.$refs.ntfc.open({
+      content: &apos;&apos;,
+      style: {
+        &apos;--background-color&apos;: &apos;blue&apos; // change the background and shadow color
+        // feel free to add custom CSS. note: css declaration must be separated by comma (,). eg.
+        // &apos;border-radius&apos;: &apos;15px&apos;,
+        // &apos;color&apos;: &apos;white&apos;
+      }
+    });
+    </hljs>
+
+
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Notification' + this.$config.base.APP_TITLE
+    }
+  },
+  methods: {
+    ntfc( type ,content ){
+      if(type==='neutral') type='blue';
+      if(type==='success') type='var(--bs-success)';
+      if(type==='error') type='var(--bs-danger)';
+      
+      this.$refs.ntfc.open({
+        content,
+        style: {
+          '--background-color': type
+        }
+      });
+    }
+  }
+}
+</script>
