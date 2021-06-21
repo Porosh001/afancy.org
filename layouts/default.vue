a0bbf524e56a4754a85956f12359ae1cf609b26d
imithu <imithu@imithu.org>
2021-06-21 23:05:11 +0600

    💄 update - layout has been updated based on layout component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,33 @@
 <template>
   <div>
-    <Nuxt />
+
+
+    <Top />
+    <div class="container-fluid my-5">
+      <div class="row gy-4">
+        <div class="col-md-3"><content-menus /></div>
+        <div class="col-md-9"><Nuxt /></div>
+      </div>
+    </div>
+    <Bottom />
+
+
+
+
+
+
+
   </div>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
+<script lang="ts">
+import Vue from 'vue';
+import Top from '@/components/layouts/default/navigation/top.vue';
+import Bottom from '@/components/layouts/default/navigation/bottom.vue';
+import content_menus from '@/components/layouts/default/content/menus.vue';
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+export default Vue.extend({
+  components: {Top, Bottom, 'content-menus': content_menus}
+})
+</script>
