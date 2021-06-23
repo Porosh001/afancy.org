3e2ae6431bc8abbc5880cb99de30ffad78e3cca4
imithu <imithu@imithu.org>
2021-06-23 16:24:43 +0600

    🌱 - docs layout has been added

---
+++ layouts/docs.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+
+    <Top />
+    <div class="container-fluid my-5">
+      <div class="row gy-4">
+        <div class="col-md-3"><content-menus /></div>
+        <div class="col-md-9"><Nuxt /></div>
+      </div>
+    </div>
+    <Bottom />
+
+
+
+
+
+
+
+  </div>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue';
+import Top from '@/components/layouts/default/navigation/top.vue';
+import Bottom from '@/components/layouts/default/navigation/bottom.vue';
+import content_menus from '@/components/layouts/default/content/menus.vue';
+
+export default Vue.extend({
+  components: {Top, Bottom, 'content-menus': content_menus}
+})
+</script>
