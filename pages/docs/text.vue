bbff2a9f3853bf929d5bfdd639f08941aa12fde0
imithu <imithu@imithu.org>
2021-07-17 14:28:40 +0600

    📝 add - fnc prefix

--- pages/docs/text.vue
+++ pages/docs/text.vue
@@ -1,68 +1,70 @@
 <template>
   <div>
 
-    <div class="h1 text-center text-primary" style='font-family: Fredericka the Great;'>Text</div>
+    <div class="h1 text-center fnc-text-primary" style='font-family: Fredericka the Great;'>Text</div>
 
-    <div class="fs-4 text-primary">text Color</div>
+    <div class="fs-4 fnc-text-primary">text Color</div>
     <div class='my-2'>Afancy has mainly 6 classes for text color</div>
     <div class="row gy-3">
-      <div class="col-4"><div class='text-primary'>.text-primary</div></div>
-      <div class="col-4"><div class='text-success'>.text-success</div></div>
-      <div class="col-4"><div class='text-danger'>.text-danger</div></div>
-      <div class="col-4"><div class='text-warning'>.text-warning</div></div>
-      <div class="col-4"><div class='text-light'>.text-light</div></div>
-      <div class="col-4"><div class='text-black bg-success'>.text-black</div></div>
+      <div class="col-4"><div class='fnc-text-primary'>.fnc-text-primary</div></div>
+      <div class="col-4"><div class='fnc-text-success'>.fnc-text-success</div></div>
+      <div class="col-4"><div class='fnc-text-danger'>.fnc-text-danger</div></div>
+      <div class="col-4"><div class='fnc-text-warning'>.fnc-text-warning</div></div>
+      <div class="col-4"><div class='fnc-text-light'>.fnc-text-light</div></div>
+      <div class="col-4"><div class='fnc-text-black fnc-bg-success'>.fnc-text-black</div></div>
     </div>
     <hljs class='mt-3' lang='css'>
     /** text color classes */
-    .text-primary
-    .text-success
-    .text-danger
-    .text-warning
-    .text-light
-    .text-black
+    .fnc-text-primary
+    .fnc-text-success
+    .fnc-text-danger
+    .fnc-text-warning
+    .fnc-text-light
+    .fnc-text-black
     </hljs>
 
-    <div class="fs-4 mt-5 text-primary">text hover Color</div>
+    <div class="fs-4 mt-5 fnc-text-primary">text hover Color</div>
     <div class='my-2'>move the mouse over the text</div>
     <div class="row gy-3">
-      <div class="col-4"><div class='hover-text-primary'>.text-primary</div></div>
-      <div class="col-4"><div class='hover-text-success'>.text-success</div></div>
-      <div class="col-4"><div class='hover-text-danger'>.text-danger</div></div>
-      <div class="col-4"><div class='hover-text-warning'>.text-warning</div></div>
-      <div class="col-4"><div class='hover-text-light'>.text-light</div></div>
-      <div class="col-4"><div class='hover-text-black bg-success'>.text-black</div></div>
+      <div class="col-4"><div class='fnc-hover-text-primary'>.fnc-text-primary</div></div>
+      <div class="col-4"><div class='fnc-hover-text-success'>.fnc-text-success</div></div>
+      <div class="col-4"><div class='fnc-hover-text-danger'>.fnc-text-danger</div></div>
+      <div class="col-4"><div class='fnc-hover-text-warning'>.fnc-text-warning</div></div>
+      <div class="col-4"><div class='fnc-hover-text-light'>.fnc-text-light</div></div>
+      <div class="col-4"><div class='fnc-hover-text-black fnc-bg-success'>.fnc-text-black</div></div>
     </div>
     <hljs class='mt-3' lang='css'>
     /** text hover color classes */
-    .hover-text-primary
-    .hover-text-success
-    .hover-text-danger
-    .hover-text-warning
-    .hover-text-light
-    .hover-text-black
+    .fnc-hover-text-primary
+    .fnc-hover-text-success
+    .fnc-hover-text-danger
+    .fnc-hover-text-warning
+    .fnc-hover-text-light
+    .fnc-hover-text-black
     </hljs>
 
-    <div class="fs-4 mt-5 text-primary">text alignment</div>
-    <div class="bg-primary text-start">text-start</div>
-    <div class="bg-success text-center">text-center</div>
-    <div class="bg-black text-end">text-end</div>
+    <div class="fs-4 mt-5 fnc-text-primary">text alignment</div>
+    <div class="fnc-bg-primary fnc-text-start">text-start</div>
+    <div class="fnc-bg-success fnc-text-center">text-center</div>
+    <div class="fnc-bg-black fnc-text-end">text-end</div>
+    <div class="fnc-bg-black fnc-text-justify">text-justify</div>
     <hljs class='mt-3' lang='css'>
     /** text alignment classes */
-    .text-start
-    .text-center
-    .text-end
+    .fnc-text-start
+    .fnc-text-center
+    .fnc-text-end
+    .fnc-text-justify
     </hljs>
 
-    <div class="fs-4 mt-5 text-primary">text highlight</div>
+    <div class="fs-4 mt-5 fnc-text-primary">text highlight</div>
     <hljs class='mt-3' lang='css'>
     /** text highlight classes */
-    .selection-primary
-    .selection-success
-    .selection-danger
-    .selection-warning
-    .selection-light
-    .selection-black
+    .fnc-selection-primary
+    .fnc-selection-success
+    .fnc-selection-danger
+    .fnc-selection-warning
+    .fnc-selection-light
+    .fnc-selection-black
     </hljs>
 
 
