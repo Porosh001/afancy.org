aae1294a3392dc68bd359b7e21566d59c71b9fc8
imithu <imithu@imithu.org>
2021-07-17 14:17:31 +0600

    📝 update - add fnc prefix

--- pages/docs/cursor.vue
+++ pages/docs/cursor.vue
@@ -1,64 +1,64 @@
 <template>
   <div>
 
-    <div class="h1 text-center text-primary" style='font-family: Fredericka the Great;'>Cursor</div>
+    <div class="h1 text-center text-primary" style='font-family: Fredericka the Great;'>fnc-cursor</div>
 
     <div class='my-2'>Move the mouse over these boxes</div>
     <div class="btn-group mb-2">
-      <button  class="btn border-primary text-light cursor-none">none</button>
-      <button  class="btn border-success text-light cursor-auto">auto</button>
-      <button  class="btn border-danger text-light  cursor-default" >default</button>
+      <button  class="btn border-primary text-light fnc-cursor-none">none</button>
+      <button  class="btn border-success text-light fnc-cursor-auto">auto</button>
+      <button  class="btn border-danger text-light  fnc-cursor-default" >default</button>
     </div>
     <br>
     <div class="btn-group mb-2">
-      <button  class="btn border-primary text-light cursor-text">text</button>
-      <button  class="btn border-success text-light cursor-pointer">pointer</button>
+      <button  class="btn border-primary text-light fnc-cursor-text">text</button>
+      <button  class="btn border-success text-light fnc-cursor-pointer">pointer</button>
     </div>
     <br>
     <div class="btn-group mb-2">
-      <button  class="btn border-primary text-light cursor-wait">wait</button>
-      <button  class="btn border-success text-light cursor-progress">progress</button>
+      <button  class="btn border-primary text-light fnc-cursor-wait">wait</button>
+      <button  class="btn border-success text-light fnc-cursor-progress">progress</button>
     </div>
     <br>
     <div class="btn-group mb-2">
-      <button  class="btn border-primary text-light cursor-move">move</button>
-      <button  class="btn border-success text-light cursor-copy">copy</button>
-      <button  class="btn border-danger text-light  cursor-col-resize" >col-resize</button>
+      <button  class="btn border-primary text-light fnc-cursor-move">move</button>
+      <button  class="btn border-success text-light fnc-cursor-copy">copy</button>
+      <button  class="btn border-danger text-light  fnc-cursor-col-resize" >col-resize</button>
     </div>
     <br>
     <div class="btn-group mb-2">
-      <button  class="btn border-primary text-light cursor-zoom-in">zoom-in</button>
-      <button  class="btn border-success text-light cursor-zoom-out">zoom-out</button>
+      <button  class="btn border-primary text-light fnc-cursor-zoom-in">zoom-in</button>
+      <button  class="btn border-success text-light fnc-cursor-zoom-out">zoom-out</button>
     </div>
     <br>
     <div class="btn-group mb-2">
-      <button  class="btn border-primary text-light cursor-help">help</button>
-      <button  class="btn border-success text-light cursor-not-allowed">not-allowed</button>
+      <button  class="btn border-primary text-light fnc-cursor-help">help</button>
+      <button  class="btn border-success text-light fnc-cursor-not-allowed">not-allowed</button>
     </div>
 
     <ntfc ref='ntfc' />
 
     <hljs lang='css'>
     /** css classes */
-    .cursor-none
-    .cursor-auto
-    .cursor-default
+    .fnc-cursor-none
+    .fnc-cursor-auto
+    .fnc-cursor-default
      ================
-    .cursor-text
-    .cursor-pointer
+    .fnc-cursor-text
+    .fnc-cursor-pointer
      ================
-    .cursor-wait
-    .cursor-progress
+    .fnc-cursor-wait
+    .fnc-cursor-progress
      ================
-    .cursor-move
-    .cursor-copy
-    .cursor-col-resize
+    .fnc-cursor-move
+    .fnc-cursor-copy
+    .fnc-cursor-col-resize
      ================
-    .cursor-zoom-in
-    .cursor-zoom-out
+    .fnc-cursor-zoom-in
+    .fnc-cursor-zoom-out
      ================
-    .cursor-help
-    .cursor-not-allowed
+    .fnc-cursor-help
+    .fnc-cursor-not-allowed
     </hljs>
 
 
@@ -74,7 +74,7 @@ export default {
   components: {hljs},
   head() {
     return {
-      title: 'Cursor' + this.$config.base.APP_TITLE
+      title: 'fnc-cursor' + this.$config.base.APP_TITLE
     }
   }
 }
